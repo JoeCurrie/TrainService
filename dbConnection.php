a21d172efcc4bc2cc94476d12c5532c68893597e
Joe Currie <joe@yovee.uk>
2016-05-16 12:43:12 +0100

    Updated Database Connection

--- dbConnection.php
+++ dbConnection.php
@@ -1,10 +1,4 @@
 <?php
-
-//Get DB Connection Details from Heroku ENV PHP Variable
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
-
 //Database Connection String
 $link mysqli_connect("uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "b0cdvdlfknn2f7i1", "t7e2dai8jpa59ak6", "dpejnuizb16bde6h") or die("Error connecting to the database: " . mysqli_error($link));
-
 ?>
