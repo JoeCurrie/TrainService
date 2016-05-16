db35dd18a5b9e66ccfb78a7a3525e6e3402b5366
Joe Currie <joe@yovee.uk>
2016-05-16 13:14:45 +0100

    Updated Display TLC - Test

--- display.php
+++ display.php
@@ -9,8 +9,12 @@ else
   include("dbConnection.php");
   $TLC = $_GET['tlc'];
   
+  echo "before";
+  
   $getTimesSQL = ("SELECT * FROM TrainTimes WHERE TLC='".$TLC."'");
   $getTimes = mysqli_query($link, $getTimesSQL);
+  
+  echo "after";
   
   while ($row = mysqli_fetch_assoc($getTimes)) {
      echo "yes";
