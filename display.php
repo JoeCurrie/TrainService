03a34e5d6c6e613e856bf5ae67009d716afcd3a8
Joe Currie <joe@yovee.uk>
2016-05-16 14:15:49 +0100

    Updated Display TLC - Test

--- display.php
+++ display.php
@@ -10,17 +10,17 @@ else
   $TLC = $_GET['tlc'];
 
   $getTimes = mysqli_query($link, "SELECT * FROM TrainTimes WHERE TLC='".$TLC."'");
+  $getStationName= mysqli_fetch_assoc($getTimes);
   
   echo "<h1>Your TLC Code is: ".$TLC."</h1>";
+  echo "<h2>".$getStationName['Origin_Name']."</h2>";
+  
   while ($row = mysqli_fetch_assoc($getTimes)) {
-     echo $row['TLC'];
      echo $row['TrainUID'];
      echo $row['Platform'];
      echo $row['Operator'];
      echo $row['Aimed_Dep_Date'];
-     echo $row['Origin_Name'];
      echo $row['Destination_Name'];
-     
   }
 }
 
