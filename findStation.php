d9d60c4eb03988d9007453cd6dd1a6da0377459d
Joe Currie <joe@yovee.uk>
2016-05-16 16:38:43 +0100

    Update findStation

--- findStation.php
+++ findStation.php
@@ -1,28 +1,18 @@
 <?php
-	// Start timer...
-	//$time_start = microtime(true);
-
 	include("httpful.phar");
 	include("dbConnection.php")
 
 	$data = array();
 	$data['error'] = false;
-
 	$url = "http://transportapi.com/v3/uk/train/stations/near.json?app_id=03bf8009&app_key=d9307fd91b0247c607e098d5effedc97&lat=" . $_POST['lat'] . "&lon=" . $_POST['long'];
-
 	$response = \Httpful\Request::get($url)->send();
 
 	if(count($response->body->stations) > 0) {
 		$data['station'] = $response->body->stations[0]->name;
-		$dt = new DateTime();
-		mysqli_query($link, "INSERT INTO `GeoHistory` (`location`, `date`) VALUES ('" . $data['station'] . "', '" . $dt->format('Y-m-d H:i:s') . "'");
+		mysqli_query($link, "INSERT INTO GeoHistory ('location', 'date') VALUES ('".$data['station']."', 'test'");
 	} else {
 		$data['error'] = true;
 	}
-
+	
 	echo json_encode($data);
-
-
-	//echo "Total execution time: " . (microtime(true) - $time_start) . "s";
-
 ?>
