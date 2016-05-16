e0ab1cdf1d4b7d8d92a9e7de72246d56ccb4870c
Joe Currie <joe@yovee.uk>
2016-05-16 12:45:07 +0100

    Updated Database Connection

--- dbConnection.php
+++ dbConnection.php
@@ -1,4 +1,4 @@
 <?php
 //Database Connection String
-$link mysqli_connect("uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "b0cdvdlfknn2f7i1", "t7e2dai8jpa59ak6", "dpejnuizb16bde6h") or die("Error connecting to the database: " . mysqli_error($link));
+$link = mysqli_connect("uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "b0cdvdlfknn2f7i1", "t7e2dai8jpa59ak6", "dpejnuizb16bde6h") or die("Error connecting to the database: " . mysqli_error($link));
 ?>
