57fdc9b66605606ba17197af354b916ed108f410
Joe Currie <joe@yovee.uk>
2016-05-16 17:25:36 +0100

    Testing SQL

--- test.php
+++ test.php
@@ -5,5 +5,5 @@ $timestamp = "Yes";
 include("dbConnection.php");
 mysqli_query($link, "INSERT INTO 'GeoHistory' ('location', 'timestamp') VALUES ('".$location."', '".$timestamp."')");
 
-echo mysqli_errno($link);
+echo mysqli_error($link);
 ?>
