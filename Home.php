f6595ac606738f8eb5ef59b2d85306736ead54db
CentoZues <Devonvanderberg@hotmail.co.uk>
2016-05-17 00:42:50 +0100

    edit

--- Home.php
+++ Home.php
@@ -60,7 +60,7 @@
 
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 <?php
-
+	echo $_SESSION['id'];
    $sql = "select * from user where id='". $_SESSION['id'] . "'";
    echo $sql . "</br>";
    $result = mysqli_query($link, $sql);
