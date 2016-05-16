8fea3024871a1990a181ca561f5f592825bae817
CentoZues <Devonvanderberg@hotmail.co.uk>
2016-05-16 13:25:21 +0100

    reg page
    page for registration

---
+++ Registration.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <title>Train Finder | Home</title>
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+
+        <!-- Required meta tags always come first -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+
+    </head>
+    <body>
+        <!-- Nav Bar -->
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">
+                        <img alt="Brand" class="img-rounded" src="img/icon3.png" width="25" height="25">
+                    </a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a data-scroll href="#searchDiv">Search</a></li>
+                    <li><a data-scroll href="#aboutDiv">About</a></li>
+                    <li><a data-scroll href="#APIDiv">API</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a data-scroll href="index.php">Home</a></li>
+                    <li><a data-scroll href="#">Log In</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container-fluid">
+            <!-- Search Bar -->
+            <div id="searchDiv" class="row">
+<!-- Sign Up Panel -->
+<div id="signup" class="panel">
+<form action = "register.php" method = "POST" class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Sign Up</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Email">Email</label>  
+  <div class="col-md-4">
+  <input id="email" name="email" type="text" placeholder="Email" class="form-control input-md">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="textinput">Confirm Email</label>  
+  <div class="col-md-4">
+  <input id="confirm_email" name="textinput" type="text" placeholder="Confirm Email" class="form-control input-md">
+    
+  </div>
+</div>
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="passwordinput">Password</label>
+  <div class="col-md-4">
+    <input id="password" ng-model="" name="password" type="password" placeholder="Password" class="form-control input-md">
+    
+  </div>
+</div>
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="passwordinput">Confirm Password</label>
+  <div class="col-md-4">
+    <input id="confirm_password" name="passwordconfirm" type="password" placeholder="Confirm Password" class="form-control input-md">
+    
+  </div>
+</div>
+
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="singlebutton"></label>
+  <div class="col-md-4">
+    <button id="singlebutton" name="singlebutton" class="btn btn-inverse">Sign Up</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+</div>
+<!-- Sign Up End -->
+<!-- Sign In Panel -->
+<div id="signin" class="panel">
+ <form action = "Home.php" method = "POST" class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Sign In</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Email">Email</label>  
+  <div class="col-md-4">
+  <input id="email" name="email" type="text" placeholder="Email" class="form-control input-md">
+    
+  </div>
+</div>
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="passwordinput">Password</label>
+  <div class="col-md-4">
+    <input id="password" name="password" type="password" placeholder="Password" class="form-control input-md">
+    
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="singlebutton"></label>
+  <div class="col-md-4">
+    <button id="singlebutton" name="singlebutton" class="btn btn-inverse">Sign In</button>
+  </div>
+</div>
+</div>
+<!-- Sign In End -->
+            </div>
+ 
+        <!-- jQuery first, then Bootstrap JS. -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/indexjs.js"></script>
+
+    </body>
+</html>
