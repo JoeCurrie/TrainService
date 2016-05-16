61ad030daff33bd893ca6bb46f4905a3a32ca090
Joe Currie <joe@yovee.uk>
2016-05-16 12:04:33 +0100

    Updated Database Connection
    Updated Database Connection details to Heroku.

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,7 @@
 <?php
-
 $link = mysqli_connect("sexpanther.eu","TrainReader","DVpxvv5Hx4VCLVv7","trainApp") or die("Error connecting to the database: " . mysqli_error($link));
 
+//$url = getenv('JAWSDB_URL');
+//$dbparts = parse_url($url);
+//$link mysqli_connect($hostname = $dbparts['host'], $username = $dbparts['user'], $password = $dbparts['pass'], $database = ltrim($dbparts['path'],'/')) or die("Error connecting to the database: " . mysqli_error($link));
 ?>
