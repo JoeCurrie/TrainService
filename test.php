6566075ebf90ad7ef35f1f186f8e87e43a2c9980
Joe Currie <joe@yovee.uk>
2016-05-16 19:58:54 +0100

    Update Test

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ $location = "Hey";
 $timestamp = "Yes";
 
 include("dbConnection.php");
-mysqli_query($link, "INSERT INTO GeoHistory ('location', 'timestamp') VALUES ('$location', '$timestamp')");
+mysqli_query($link, "INSERT INTO GeoHistory (location, timestamp) VALUES ('$location', '$timestamp')");
 
 echo mysqli_error($link);
 ?>
