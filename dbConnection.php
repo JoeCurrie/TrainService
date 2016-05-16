833a5d3afa5a32b86bb51bc26175e18eabbb1255
Joe Currie <joe@yovee.uk>
2016-05-16 12:37:04 +0100

    Updated Database Connection

--- dbConnection.php
+++ dbConnection.php
@@ -5,10 +5,7 @@ $url = getenv('JAWSDB_URL');
 $dbparts = parse_url($url);
 
 //Database Connection String
-//$link mysqli_connect($hostname = $dbparts['host'], $username = $dbparts['user'], $password = $dbparts['pass'], $database = ltrim($dbparts['path'],'/'));
-
-$link mysqli_connect($hostname = $dbparts['hosjt'], $username = $dbparts['usejr'], $password = $dbparts['pasjs'], $database = ltrim($dbparts['path'],'/'));
-
+$link mysqli_connect($hostname = $dbparts['host'], $username = $dbparts['user'], $password = $dbparts['pass'], $database = ltrim($dbparts['path'],'/'));
 
 //Check for Errors
 if ($link->connect_error) {
