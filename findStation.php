e1397e56af6c205a2959c12bceab1c9312f5d78f
Joe Currie <joe@yovee.uk>
2016-05-16 17:17:21 +0100

    Update findStation

--- findStation.php
+++ findStation.php
@@ -1,19 +1,17 @@
 <?php
 	include("httpful.phar");
-
 	$data = array();
 	$data['error'] = false;
 	$url = "http://transportapi.com/v3/uk/train/stations/near.json?app_id=03bf8009&app_key=d9307fd91b0247c607e098d5effedc97&lat=" . $_POST['lat'] . "&lon=" . $_POST['long'];
 	$response = \Httpful\Request::get($url)->send();
-
 	if(count($response->body->stations) > 0) {
 		$data['station'] = $response->body->stations[0]->name;
 		
-		$location = $data['station'];
+		$location = 'test';
 		$timestamp = 'test';
 		
 		include("dbConnection.php");
-		$addHistory = mysqli_query( $link, "INSERT INTO 'Stations' ('TLC', 'Station', 'NLC') VALUES ('".$location."', '".$timestamp."', '45')" );
+		$addHistory = mysqli_query($link, "INSERT INTO 'GeoHistory' ('location', 'timestamp') VALUES ('".$location."', '".$timestamp."')");
 	} else {
 		$data['error'] = true;
 	}
