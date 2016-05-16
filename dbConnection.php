17eb6944853bf92bef493189be5d080b4932f24f
Joe Currie <joe@yovee.uk>
2016-05-16 12:42:36 +0100

    Updated Database Connection

--- dbConnection.php
+++ dbConnection.php
@@ -5,11 +5,6 @@ $url = getenv('JAWSDB_URL');
 $dbparts = parse_url($url);
 
 //Database Connection String
-$link mysqli_connect("uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "b0cdvdlfknn2f7i1", "t7e2dai8jpa59ak6", "dpejnuizb16bde6h");
-
-//Check for Errors
-if ($link->connect_error) {
-    die("Connection failed: " . $link->connect_error);
-}
+$link mysqli_connect("uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "b0cdvdlfknn2f7i1", "t7e2dai8jpa59ak6", "dpejnuizb16bde6h") or die("Error connecting to the database: " . mysqli_error($link));
 
 ?>
