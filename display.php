5c7f2b78e54a6bd02e9b42dc7aecb79792e9f712
Joe Currie <joe@yovee.uk>
2016-05-16 13:54:43 +0100

    Updated Display TLC - Test

--- display.php
+++ display.php
@@ -12,7 +12,7 @@ else
   $getTimes = mysqli_query($link, "SELECT * FROM TrainTimes WHERE TLC='".$TLC."'");
   
   while ($row = mysqli_fetch_assoc($getTimes)) {
-     echo "yes";
+     echo $row['ID'];
   }
 }
 
