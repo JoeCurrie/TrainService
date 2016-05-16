c4d99fb17df33e0050d3cd9797f24032f9f9cb7e
Joe Currie <joe@yovee.uk>
2016-05-16 14:19:12 +0100

    Updated Display TLC - Test

--- display.php
+++ display.php
@@ -1,27 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Train Finder | Display Times</title>
 
-if(!(isset($_GET['tlc'])))
-{
-  header("Location: index.php");
-}
-else
-{
-  include("dbConnection.php");
-  $TLC = $_GET['tlc'];
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon">
 
-  $getTimes = mysqli_query($link, "SELECT * FROM TrainTimes WHERE TLC='".$TLC."'");
-  $getStationName= mysqli_fetch_assoc($getTimes);
-  
-  echo "<h1>Your TLC Code is: ".$TLC."</h1>";
-  echo "<h2>".$getStationName['Origin_Name']."</h2>";
-  
-  while ($row = mysqli_fetch_assoc($getTimes)) {
-     echo $row['TrainUID'];
-     echo $row['Platform'];
-     echo $row['Operator'];
-     echo $row['Aimed_Dep_Date'];
-     echo $row['Destination_Name'];
-  }
-}
-
-?>
+        <!-- Required meta tags always come first -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+    </head>
+    <body>
+      <?php
+      
+      if(!(isset($_GET['tlc'])))
+      {
+        header("Location: index.php");
+      }
+      else
+      {
+        include("dbConnection.php");
+        $TLC = $_GET['tlc'];
+      
+        $getTimes = mysqli_query($link, "SELECT * FROM TrainTimes WHERE TLC='".$TLC."'");
+        $getStationName= mysqli_fetch_assoc($getTimes);
+        
+        echo "<h1>Your TLC Code is: ".$TLC."</h1>";
+        echo "<h2>".$getStationName['Origin_Name']."</h2>";
+        
+        while ($row = mysqli_fetch_assoc($getTimes)) {
+           echo $row['TrainUID'];
+           echo $row['Platform'];
+           echo $row['Operator'];
+           echo $row['Aimed_Dep_Date'];
+           echo $row['Destination_Name'];
+        }
+      }
+      
+      ?>
+    </body>
+</html>
