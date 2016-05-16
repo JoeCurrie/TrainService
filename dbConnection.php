9aace0bbe0a120a79c1a39d96c3765d143949308
Joe Currie <joe@yovee.uk>
2016-05-16 12:10:17 +0100

    Updated Error Handler & Comments
    Updated the Error Handler for the Database Connection & added Comments.

--- dbConnection.php
+++ dbConnection.php
@@ -1,7 +1,14 @@
 <?php
-$link = mysqli_connect("sexpanther.eu","TrainReader","DVpxvv5Hx4VCLVv7","trainApp") or die("Error connecting to the database: " . mysqli_error($link));
 
-//$url = getenv('JAWSDB_URL');
-//$dbparts = parse_url($url);
-//$link mysqli_connect($hostname = $dbparts['host'], $username = $dbparts['user'], $password = $dbparts['pass'], $database = ltrim($dbparts['path'],'/')) or die("Error connecting to the database: " . mysqli_error($link));
+//Get DB Connection Details from Heroku ENV PHP Variable
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+//Database Connection String
+$link mysqli_connect($hostname = $dbparts['host'], $username = $dbparts['user'], $password = $dbparts['pass'], $database = ltrim($dbparts['path'],'/'));
+
+//Check for Errors
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+}
 ?>
