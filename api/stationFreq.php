a86cdd2b120d02922ba58a2074fb727b440ea548
HowlingWind <rahucks@gmail.com>
2016-05-17 04:27:05 +0100

    Complete API endpoint

--- api/stationFreq.php
+++ api/stationFreq.php
@@ -11,20 +11,20 @@ $data = array();
 
 // Check if the APIKey exists...
 if (isset($_GET['apiKey']) && ($_GET['apiKey'] != "")) {
-	echo "Key is included</br>";
+	//echo "Key is included</br>";
 	$apiCheckSQL = "SELECT * FROM `user` WHERE `apiKey` = '" . $_GET['apiKey'] . "'";
-	echo $apiCheckSQL;
+	//echo $apiCheckSQL;
 	$apiCheckresult = mysqli_query($link, $apiCheckSQL) or die(mysqli_error($link));
 	// Check the API key is valid
 	if(mysqli_num_rows($apiCheckresult) == 1) {
-		echo "Key is valid</br>";
+		//echo "Key is valid</br>";
 		// Check if the station var is set...
 		if (isset($_GET['tlc']) && ($_GET['tlc'] != "")) {
-			echo "TLC is included</br>";
+			//echo "TLC is included</br>";
 			// Check if the station var is valid
 			$result = mysqli_query($link, "SELECT `station` FROM `Stations` WHERE `TLC` = '" . strtoupper($_GET['tlc']) . "'") or die(mysqli_error());
 			if(mysqli_num_rows($result) != 0) {
-				echo "Key is valid</br>";
+				//echo "TLC is valid</br>";
 				$apiResult = mysqli_query($link, "SELECT * FROM `StationLineHistory` WHERE `TLC` = '" . strtoupper($_GET['tlc']) . "'") or die(mysqli_error());
 				while ($row = mysqli_fetch_assoc($apiResult)){
 					$tmp = array();
