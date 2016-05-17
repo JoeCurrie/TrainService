1fcaddc0de46f8552041cbe60d5c5cb225b6e4d0
HowlingWind <rahucks@gmail.com>
2016-05-17 04:18:23 +0100

    if statement adjust

--- api/stationFreq.php
+++ api/stationFreq.php
@@ -15,14 +15,14 @@ if (isset($_GET['apiKey']) && ($_GET['apiKey'] != "")) {
 	echo $apiCheckSQL;
 	$result = mysqli_query($link, $apiCheckSQL) or die(mysqli_error($link));
 	// Check the API key is valid
-	if(!mysqli_num_rows($result) == 0) {
+	if(mysqli_num_rows($result) != 0) {
 		echo "Key is valid</br>";
 		// Check if the station var is set...
 		if (isset($_GET['tlc']) && ($_GET['tlc'] != "")) {
 			echo "TLC is included</br>";
 			// Check if the station var is valid
 			$result = mysqli_query($link, "SELECT `station` FROM `Stations` WHERE `TLC` = '" . strtoupper($_GET['tlc']) . "'") or die(mysqli_error());
-			if(!mysqli_num_rows($result) == 0) {
+			if(mysqli_num_rows($result) != 0) {
 				echo "Key is valid</br>";
 				$apiResult = mysqli_query($link, "SELECT * FROM `StationLineHistory` WHERE `TLC` = '" . strtoupper($_GET['tlc']) . "'") or die(mysqli_error());
 				while ($row = mysqli_fetch_assoc($apiResult)){
