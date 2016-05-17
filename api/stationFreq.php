c46618fc56fa6019d4d5e1693ca1ac4675708069
HowlingWind <rahucks@gmail.com>
2016-05-17 04:12:43 +0100

    API update

--- api/stationFreq.php
+++ api/stationFreq.php
@@ -11,7 +11,9 @@ $data = array();
 // Check if the APIKey exists...
 if (isset($_GET['apiKey']) && ($_GET['apiKey'] != "")) {
 	echo "Key is included</br>";
-	$result = mysqli_query($link, "SELECT `apiKey` FROM `user` WHERE `apiKey` = '" . $_GET['apiKey'] . "'");
+	$apiCheckSQL = "SELECT `apiKey` FROM `user` WHERE `apiKey` = '" . $_GET['apiKey'] . "'";
+	echo $apiCheckSQL;
+	$result = mysqli_query($link, $apiCheckSQL) or die(mysqli_error());
 	// Check the API key is valid
 	if(!mysqli_num_rows($result) == 0) {
 		echo "Key is valid</br>";
@@ -19,10 +21,10 @@ if (isset($_GET['apiKey']) && ($_GET['apiKey'] != "")) {
 		if (isset($_GET['tlc']) && ($_GET['tlc'] != "")) {
 			echo "TLC is included</br>";
 			// Check if the station var is valid
-			$result = mysqli_query($link, "SELECT `station` FROM `Stations` WHERE `TLC` = '" . strtoupper($_GET['tlc']) . "'");
+			$result = mysqli_query($link, "SELECT `station` FROM `Stations` WHERE `TLC` = '" . strtoupper($_GET['tlc']) . "'") or die(mysqli_error());
 			if(!mysqli_num_rows($result) == 0) {
 				echo "Key is valid</br>";
-				$apiResult = mysqli_query($link, "SELECT * FROM `StationLineHistory` WHERE `TLC` = '" . strtoupper($_GET['tlc']) . "'");
+				$apiResult = mysqli_query($link, "SELECT * FROM `StationLineHistory` WHERE `TLC` = '" . strtoupper($_GET['tlc']) . "'") or die(mysqli_error());
 				while ($row = mysqli_fetch_assoc($apiResult)){
 					$tmp = array();
 					$tmp['Operator'] = $row['Operator'];
@@ -32,19 +34,15 @@ if (isset($_GET['apiKey']) && ($_GET['apiKey'] != "")) {
 
 			} else {
 				$error['description'] = "The station specified in the requested URL does not exist. Read the documents at https://trainservice.herokuapp.com/#APIDiv";
-				mysqli_error();
 			}
 		} else {
 			$error['description'] = "The TLC code is not included in the requested URL. Read the documents at https://trainservice.herokuapp.com/#APIDiv";
-			mysqli_error();
 		}
 	} else {
 		$error['description'] = "The API key included in the requested URL is not valid. Read the documents at https://trainservice.herokuapp.com/#APIDiv";
-		mysqli_error();
 	}
 } else {
 	$error['description'] = "The API key is not included in the requested URL. Read the documents at https://trainservice.herokuapp.com/#APIDiv";
-	mysqli_error();
 }
 
 if (isset($error['description'])) {
